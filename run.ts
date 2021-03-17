34d56512b5f4d1fdc056c15a7cfba70a8ca06a1f
Michiel de Jong <michiel@unhosted.org>
2021-03-17 18:54:36 +0100

    adding some types

--- run.ts
+++ run.ts
@@ -3,7 +3,7 @@ import { mutationToCategory } from './src/expenseCategories'
 import { AccountHistoryChunk } from './src/Ledger'
 import { parseAsnbankCsv } from './src/parsers/asnbank-csv'
 import { parseAsnbankMt940 } from './src/parsers/asnbank-mt940'
-import { parseHours } from './src/parsers/hours'
+import { HoursProject, parseHours } from './src/parsers/hours'
 import { parseIngCreditcardScrape } from './src/parsers/ing-creditcard-scrape'
 import { parseIngbankCsv } from './src/parsers/ingbank-csv'
 import { parsePaypalCsv } from './src/parsers/paypal-csv'
@@ -13,27 +13,25 @@ import { parseWieBetaaltWat } from './src/parsers/wiebetaaltwat'
 const dataRoot = require(process.env.DATA_ROOT)
 
 let accountHistoryChunks: AccountHistoryChunk[] = []
-const parsers = {
+const parsers: { [parserName: string]: (args: { fileBuffer: Buffer | string, fileId: string }) => AccountHistoryChunk } = {
   'asnbank-csv': parseAsnbankCsv,
   'asnbank-mt940': parseAsnbankMt940,
-  'hours': parseHours,
   'ing-creditcard-scrape': parseIngCreditcardScrape,
   'ingbank-csv': parseIngbankCsv,
   'paypal-csv': parsePaypalCsv,
   'wiebetaaltwat': parseWieBetaaltWat,
 }
 
-Object.keys(dataRoot.files).forEach(fileName => {
+Object.keys(dataRoot.files).forEach((fileName: string) => {
   const fileBuffer = readFileSync(fileName, 'utf8')
   const parser = parsers[dataRoot.files[fileName]]
-  const chunk: AccountHistoryChunk = parser({ fileBuffer, fileId: fileName, dataRoot })
+  const chunk: AccountHistoryChunk = parser({ fileBuffer, fileId: fileName })
   accountHistoryChunks.push(chunk)
     console.log(`Parsed ${chunk.importedFrom[0].fileId} with ${chunk.mutations.length} statements`)
 })
 
-Object.keys(dataRoot.hours).forEach(year => {
-  const parser = parsers.hours;
-  const chunk: AccountHistoryChunk = parser({ hours: dataRoot.hours[year], year: parseInt(year) })
+Object.keys(dataRoot.hours).forEach((yearStr: string) => {
+  const chunk: AccountHistoryChunk = parseHours({ hours: dataRoot.hours[yearStr], year: parseInt(yearStr) })
   accountHistoryChunks.push(chunk)
     console.log(`Parsed ${chunk.importedFrom[0].fileId} with ${chunk.mutations.length} statements`)
 })
@@ -43,7 +41,7 @@ accountHistoryChunks[0].mutations.map(mutation => {
   // console.log(category, mutation);
 });
 
-function printMonthlyTotals(account) {
+function printMonthlyTotals(account: string): void {
   accountHistoryChunks.filter(chunk => chunk.account === account).forEach(chunk => {
     const total = chunk.mutations.map(mutation => mutation.amount).reduce((accumulator: number, currentValue: number) => accumulator + currentValue, 0)
     console.log('chunk!', total)
