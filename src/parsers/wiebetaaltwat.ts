1b3981495ca72a8563a6870e9db56c5285ad43a3
Michiel de Jong <michiel@unhosted.org>
2021-03-20 10:22:44 +0100

    WieBetaaltWat import improved

--- src/parsers/wiebetaaltwat.ts
+++ src/parsers/wiebetaaltwat.ts
@@ -1,10 +1,30 @@
 import { v4 as uuidV4 } from 'uuid'
 import { AccountHistoryChunk, Balance, HalfTrade, ImportDetails, WorldLedgerMutation } from '../Ledger';
-import { toDate } from "./asnbank-csv";
 import { parseGeneric } from './parseGeneric';
 
 const PARSER_NAME = 'wiebetaaltwat';
 const PARSER_VERSION = 'v0.1.0';
+
+function toDate(str: string) {
+  // e.g. 30-12-19
+  const dateStrMatches = /(.+)-(.*)-(.*)/g.exec(str)
+  if (!dateStrMatches) {
+    return null
+  }
+  const day = parseInt(dateStrMatches[1], 10);
+  const month = parseInt(dateStrMatches[2], 10) - 1;
+  const year = 2000 + parseInt(dateStrMatches[3], 10);
+  const date = new Date(Date.UTC(year, month, day));
+  console.log('toDate', str, year, month, day, date);
+  return date
+}
+
+// WieBetaaltWat has several formats:
+// Alice // tickets //  € 100,00 // 16-02-18 // Bob, Charlie, Dan
+// Alice // tickets //  € 100,00 // 16-02-18 // Bob, Charlie (50,00), Dan
+// Alice // tickets //  € 100,00 // 16-02-18 // Bob, Charlie (50,00), Dan (20,00)
+// Alice // tickets //  € 100,00 // 16-02-18 // Bob (30,00), Charlie (50,00), Dan (20,00)
+// Alice // tickets //  € 100,00 // 16-02-18 // Bob, Charlie (2x), Dan
 
 function parseWbwToParts(amount, tos) {
   let total = 0;
@@ -24,7 +44,7 @@ function parseWbwToParts(amount, tos) {
   Object.keys(parts).forEach(name => {
     parts[name] = parts[name] * factor;
   });
-  // console.log('parsed', parts);
+  console.log('parsed', parts);
   return parts;
 }
 
@@ -59,21 +79,23 @@ function parseWbwTo(amount, tos) {
 
 function parseLines(lines: string[]): WorldLedgerMutation[] {
   let cursor = 0;
-  const entries = [];
+  const mutations = [];
   do {
     const entryAmount = parseFloat(lines[cursor+2].split(' ')[1].replace(',', '.'));
     const amounts = parseWbwTo(entryAmount, lines[cursor+4].split(', '));
     let sanityCheck = 0;
     Object.keys(amounts).forEach(name => {
-      entries.push({
+      mutations.push(new WorldLedgerMutation({
         from: lines[cursor],
-        description: lines[cursor+1],
         to: name,
         amount: amounts[name],
-        date: lines[cursor+3],
-        fullInfo: lines.slice(cursor, cursor + 6),
-        // impliedBy: `${scrapeFileUrl}#L${cursor + 1}-L${cursor + 5}` // First line is line 1
-      });
+        date: toDate(lines[cursor+3]),
+        unit: 'EUR',
+        data: {
+          description: lines[cursor+1],
+          fullInfo: lines.slice(cursor, cursor + 6),
+        }
+      }));
       sanityCheck += amounts[name];
     });
     if (Math.abs(sanityCheck - entryAmount) > 0.01) {
@@ -82,31 +104,16 @@ function parseLines(lines: string[]): WorldLedgerMutation[] {
     }
     cursor += 6;
   } while(cursor < lines.length);
-  return entries.map(obj => {
-    const date = toDate(obj.date);
-    return new WorldLedgerMutation({
-      from: obj.from,
-      to: obj.to,
-      date,
-      amount: obj.amount,
-      unit: 'EUR',
-      data: {
-        // halfTradeId: `wiebetaaltwat-${obj.date}-${obj.from}-${obj.to}-${uuidV4()}`,
-        description: obj.description,
-        impliedBy: obj.impliedBy,
-        fullInfo: obj.fullInfo
-      }
-    })
-  });
+  return mutations;
 }
 
-export function parseWieBetaaltWat ({ fileBuffer, fileId }) {
+export function parseWieBetaaltWat ({ fileBuffer, fileId, details }) {
   return parseGeneric({
     fileBuffer,
     fileId,
     parserName: PARSER_NAME,
     parserVersion: PARSER_VERSION,
-    account: 'me-wie-betaalt-wat',
+    account: details.account,
     parseLines
   });
 }
